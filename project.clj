f441176a0fb670542db9468425519569772be95b
Micah <micah@8thlight.com>
2011-02-08 21:58:32 -0600

    cleaning project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gaeshi "1.0.0-SNAPSHOT"
+(defproject gaeshi "0.1.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [ring/ring-servlet "0.3.5"]
@@ -6,12 +6,8 @@
                  [fresh "1.0.1"]]
   :dev-dependencies [[speclj "1.2.0"]
                      [junit "4.8.2"]
-                     [com.google.appengine/appengine-api-labs "1.4.0"]
-                     [com.google.appengine/appengine-api-stubs "1.4.0"]
-                     [com.google.appengine/appengine-local-runtime "1.4.0"]
-                     [com.google.appengine/appengine-local-runtime-shared "1.4.0"]
-                     [com.google.appengine/appengine-testing "1.4.0"]]
+                     [lein-clojars "0.6.0"]]
   :test-path "spec/"
   :java-source-path "src/"
-  :repositories { "local" "file://m2"}
+  :repositories {"local" "file://m2"}
   )

08e9334dadd45789120d757903409056285ac34d
Micah <micah@8thlight.com>
2011-06-01 11:37:23 -0500

    added fake Java http utilities

---
+++ tsukuri/src/gaeshi/http/Pair.java
@@ -0,0 +1,13 @@
+package gaeshi.http;
+
+public class Pair
+{
+  public String first;
+  public String second;
+
+  public Pair(String first, String second)
+  {
+    this.first = first;
+    this.second = second;
+  }
+}

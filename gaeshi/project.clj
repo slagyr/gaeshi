2dca9ec42dbed5099fcd40d0790d73b6f7ba0cd1
Micah <micah@8thlight.com>
2012-04-25 13:28:22 -0500

    updated to joodo 0.7.2

--- gaeshi/project.clj
+++ gaeshi/project.clj
@@ -1,11 +1,11 @@
-(defproject gaeshi/gaeshi "0.7.2"
+(defproject gaeshi/gaeshi "0.7.3"
   :description "Runtime library for Gaeshi, a Clojure framework for Google App Engine apps."
   :license {:name "The MIT License"
             :url "file://LICENSE"
             :distribution :repo
             :comments "Copyright © 2011-2012 Micah Martin All Rights Reserved."}
   :repositories {"releases" "http://gaeshi-mvn.googlecode.com/svn/trunk/releases/"}
-  :dependencies [[joodo "0.7.1"]
+  :dependencies [[joodo "0.7.2"]
                  [com.google.appengine/appengine-api-1.0-sdk "1.6.2.1"]]
   :dev-dependencies [[speclj "2.1.1"]
                      [gaeshi/gaeshi-dev "0.7.0"]]
